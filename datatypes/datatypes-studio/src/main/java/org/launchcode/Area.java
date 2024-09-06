e601a43fa514450066043af37035901d0138194a
Anna Valerio <anna.j.neff@gmail.com>
2024-09-05 19:27:03 -0500

    Java Studio 1

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,15 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    static public void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        double radius = input.nextDouble();
+
+        double area = radius * radius * 3.14;
+        input.close();
+        System.out.println("The area of a circle of radius "+ radius + " is " + area + ".");
+    }
+}
