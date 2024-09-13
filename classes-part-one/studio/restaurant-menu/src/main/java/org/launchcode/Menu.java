32374a72757485652eeadc19997bb4610b21a0ec
Anna Valerio <anna.j.neff@gmail.com>
2024-09-12 20:30:58 -0500

    Classes Pt 1 Studio

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Menu {
+    private LocalDate dateUpdated;
+    private ArrayList<MenuItem> appetizers = new ArrayList<>();
+    private ArrayList<MenuItem> mainCourses = new ArrayList<>();
+    private ArrayList<MenuItem> desserts = new ArrayList<>();
+
+}
+// Allow for default constructor public ArrayList<MenuItem> getMenuItems() { return menuItems; } }
---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+public class MenuItem {
+    private String itemName;
+    private String description;
+    private boolean isNew;
+    private int price;
+    private int category;
+
+
+
+}
