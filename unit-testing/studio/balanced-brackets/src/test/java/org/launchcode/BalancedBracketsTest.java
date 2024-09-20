81433b5963f7dfa472a25b1ea8d9ecdefd39d963
Anna Valerio <anna.j.neff@gmail.com>
2024-09-19 20:01:01 -0500

    Java Unit Testing Studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,89 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void LaunchCodeReturnsInBrackets() {
+        String result = "[LaunchCode]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void CodeReturnsInBrackets() {
+        String result = "Launch[Code]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void EmptyStringReturnsNoBrackets() {
+        String result = "";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ReturnsWithOneOpenBracket() {
+        String result = "[LaunchCode";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ReturnsBackwardsBracketInWord() {
+        String result = "Launch]Code[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ReturnsLeftBracketOnly() {
+        String result = "[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ManyBalancedBrackets() {
+        String result = "[[[]]]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+
+    @Test
+    public void ManyUnbalancedBrackets() {
+        String result = "[[[]]";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+
+    @Test
+    public void ManyOutOfOrderBrackets() {
+        String result = "][[[]]";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ManyOrderedBrackets() {
+        String result = "[][][]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ManyOrderedBracketsWithWords() {
+        String result = "[Launch][Code][What]evers";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ConcatenatedBrackets() {
+        String result = "[][][]" + "Hello World";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(result));
+    }
+
+    @Test
+    public void ConcatenatedOpenBrackets() {
+        String result = "[][][]" + "Hello World[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(result));
+    }
 }
