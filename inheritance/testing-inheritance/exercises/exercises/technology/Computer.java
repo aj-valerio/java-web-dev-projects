f0ce3b62aa3b6f907111caf50ad7cc25fadc5911
Anna Valerio <anna.j.neff@gmail.com>
2024-09-22 15:55:19 -0500

    Inheritance exercises part 1

---
+++ inheritance/testing-inheritance/exercises/exercises/technology/Computer.java
@@ -0,0 +1,46 @@
+package exercises.technology;
+
+public class Computer {
+    private String deviceName;
+    private final String owner = "AJ";
+    private String make;
+    private String model;
+
+    public Computer(String aDeviceName, String aMake, String aModel) {
+        this.deviceName = aDeviceName;
+        this.make = aMake;
+        this.model = aModel;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void describeDevice(){
+        System.out.println(this.deviceName + " is " + this.owner + "'s " + this.make + " " + this.model + ".");
+    }
+}
